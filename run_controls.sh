08cb59ba15018273f090ed86dc478a0bbce7dfec
albornet <alban.bornet@gmail.com>
2024-10-01 09:49:51 +0200

    debug removed

--- run_controls.sh
+++ run_controls.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=private-teodoro-gpu
 #SBATCH --ntasks=1
 #SBATCH --gpus-per-task=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=96gb
 #SBATCH --time=6-00:00:00
 #SBATCH --output=/home/users/b/borneta/hai_project/logs/run_controls/job_%j.txt
